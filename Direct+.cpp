0304774a5fc7885b16108c7befabbe2d955e051e
Niamh Driscoll <niamhmolson@outlook.com>
2025-07-16 14:39:37 -0700

    Uh

--- Direct+.cpp
+++ Direct+.cpp
@@ -1,20 +1,19 @@
-// Direct+.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <fstream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+	std::cout << "Samuel hi" << std::endl;
+	std::ifstream file("target.dp");
+
+	if (!file) {
+		std::cerr << "Error opening file." << std::endl;
+		return 1;
+	}
+	std::string line;
+	while (std::getline(file, line)) {
+		std::cout << line << std::endl;
+	}
+	return 0;
+	
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
